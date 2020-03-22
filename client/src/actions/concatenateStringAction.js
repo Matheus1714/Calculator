298c9a992321323984f00da86f15adabb3b57a95
Matheus Mota <matheussilvamartins1714@gmail.com>
2020-03-22 11:44:50 -0300

    update expression value

--- client/src/actions/concatenateStringAction.js
+++ client/src/actions/concatenateStringAction.js
@@ -1,6 +1,10 @@
+import { inputTextAction } from './inputTextAction'
+
 export function concatenateStringAction(character){
     let oldExpression = localStorage.getItem('expression')
     let newExpression = oldExpression + character
 
     localStorage.setItem('expression', newExpression)
+
+    inputTextAction(newExpression)
 }
