c4cf13172d7d5d4b88777a1a0646f0813354e106
Matheus Mota <matheussilvamartins1714@gmail.com>
2020-03-22 11:45:55 -0300

    input console

--- client/src/components/consoleComponent.js
+++ client/src/components/consoleComponent.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField';
 
 export default function ConsoleComponent(){
     return(
         <Grid item xs={12}>
-            <h1>{localStorage.getItem('expression')}</h1>
+            <TextField id="expressionText" />
         </Grid>
     )
 }
