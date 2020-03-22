7d4d582372f10ade06d123ef4a4310018925b3b9
Matheus Mota <matheussilvamartins1714@gmail.com>
2020-03-22 11:46:21 -0300

    change screen length

--- client/src/styles/calculatorComponentStyles.js
+++ client/src/styles/calculatorComponentStyles.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles'
 export const useStyles = makeStyles({
     root: {
       minWidth: 275,
-      maxWidth: 600,
+      maxWidth: 350,
       minHeight: 400,
       flexGrow: 1,
       alignItems: 'center',
