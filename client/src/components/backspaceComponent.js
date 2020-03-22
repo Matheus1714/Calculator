c74933f778ebb3a57a3379edf958957ece603e2d
Matheus Mota <matheussilvamartins1714@gmail.com>
2020-03-22 11:45:44 -0300

    button action back space

--- client/src/components/backspaceComponent.js
+++ client/src/components/backspaceComponent.js
@@ -2,11 +2,21 @@ import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import BackspaceIcon from '@material-ui/icons/Backspace'
 import Button from '@material-ui/core/Button'
+import { removeLastElement } from '../actions/removeLastElement'
 
 export default function BackspaceComponent(){
+    const handleClick = async () => {
+        try{
+            removeLastElement()
+        }catch(err){    
+            console.error(err)
+        }
+    }
     return(
         <Grid item xs={3}>
-            <Button>
+            <Button
+                onClick={() => handleClick()}
+            >
                 <BackspaceIcon/>
             </Button>
         </Grid>
